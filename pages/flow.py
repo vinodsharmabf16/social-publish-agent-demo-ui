42e17e294fef92d7270235b25cb412d5eed7b814
Divyanshu Upadhyay <divyanshu@ip-192-168-77-88.us-west-1.compute.internal>
2025-05-21 17:11:47 +0530

    changed flow image

--- pages/flow.py
+++ pages/flow.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 def render():
     gr.Markdown("### Workflow Flowchart")
-    gr.Image("/Users/divyanshu/Documents/new/backend/SocialPublishingAgent.png", type="filepath", elem_id="flow-image")
+    gr.Image("./backend/SocialPublishingAgent.png", type="filepath", elem_id="flow-image")
     gr.HTML("""
     <style>
         #flow-image img {
